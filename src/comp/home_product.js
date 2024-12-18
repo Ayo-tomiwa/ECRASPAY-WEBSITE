095dd3f5167ab5a12a250959273a865f8a457ca6
Ayotomiwa Onifara <oyindml1@gmail.com>
2024-12-18 11:59:49 +0100

    Nav and hero section completed

---
+++ src/comp/home_product.js
@@ -0,0 +1,69 @@
+const Homeproduct=
+[
+    {
+        id: 1,
+        Name: 'product1',
+        price: '26',
+        // image: ''
+        cat: '',
+        type: 'new',
+    },
+    {
+        id: 2,
+        Name: 'product2',
+        price: '96',
+        // image: ''
+        cat: '',
+        type: 'new',
+    },
+    {
+        id: 3,
+        Name: 'product3',
+        price: '236',
+        // image: ''
+        cat: '',
+        type: 'new',
+    },
+    {
+        id: 4,
+        Name: 'product4',
+        price: '98',
+        // image: ''
+        cat: '',
+        type: 'featured',
+    },
+    {
+        id: 5,
+        Name: 'product5',
+        price: '59',
+        // image: ''
+        cat: '',
+        type: 'featured',
+    },
+    {
+        id: 6,
+        Name: 'product6',
+        price: '67',
+        // image: ''
+        cat: '',
+        type: 'top',
+    },
+    {
+        id: 7,
+        Name: 'product7',
+        price: '76',
+        // image: ''
+        cat: '',
+        type: 'top',
+    },
+    {
+        id: 8,
+        Name: 'product8',
+        price: '87',
+        // image: ''
+        cat: '',
+        type: 'top',
+    },
+]
+
+export default Homeproduct
